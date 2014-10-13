ed3ecd324bf50f45159fef197985d19f45819413
connor.joyce <connor.joyce@MU-C3-13.mullen.pvt.k12.co.us>
2014-10-13 12:34:31 -0700

    outlined comments with how to proceed with the class

--- VendingMachine/src/Cashier.java
+++ VendingMachine/src/Cashier.java
@@ -1,5 +1,34 @@
 
 public class Cashier
 	{
-
+	private int currentCents;
+	public Cashier(int c)
+		{
+		currentCents = c;
+		}
+	public int[] addMoney(int value)
+		{
+		//take the money that the user inputs into the scanner and add it in
+		}
+	public int[] currentValue()
+		{
+		// display the cost? 
+		}
+	public void purchase(int price)
+		{
+		ProductManager.get(item number).getPrice();
+		// make sure you have the variable that will get the correct product
+		}
+	public String returnChange(int[] money)
+		{
+		//return the difference between money input and cost of product
+		}
+	public static String showCash(int[] money)
+		{
+		//display the money input into the machine
+		}
+	public static int toPennies(int[] value)
+		{
+		// convert the change into pennies
+		}
 	}
