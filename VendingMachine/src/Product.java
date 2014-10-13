9a6a078c74e25dbcb769c1f4cda86291d6a86c72
connor.joyce <connor.joyce@MU-C3-13.mullen.pvt.k12.co.us>
2014-10-13 12:12:41 -0700

    created the product constructor with the parameters of the item name and cost in pennies

--- VendingMachine/src/Product.java
+++ VendingMachine/src/Product.java
@@ -1,5 +1,28 @@
 
 public class Product
 	{
-
+	private String snackName;
+	private int costInCents;
+	public Product(String name, int price)
+		{
+		snackName = name;
+		costInCents = price;
+		}
+	public String getSnackName()
+		{
+		return snackName;
+		}
+	public int getCostInCents()
+		{
+		return costInCents;
+		}
+	public void setSnackName(String snackName)
+		{
+		this.snackName = snackName;
+		}
+	public void setCostInCents(int costInCents)
+		{
+		this.costInCents = costInCents;
+		}
+	
 	}
